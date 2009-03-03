1ebae83c8d42e459098ac73aaa02317a1b04f980
Nolan Evans <nolanevans@nolan-evanss-macbook-pro.local>
2009-03-03 11:28:04 -0800

    simplifying the explorer

--- lib/magellan/explorer.rb
+++ lib/magellan/explorer.rb
@@ -4,25 +4,29 @@ require 'magellan/extensions/http_response'
 
 module Magellan
   class Explorer
-    def initialize(url, &block)
-      @url=url
-      @block=block
+
+    def explore(url)
+      response = web_response(url)
+      Result.new(response.code,response.linked_resources)
     end
     
-    def explore
-      response = web_response
-      @block.call(response.code,response.linked_resources)
-    end
-    
-    def web_response
+    def web_response(url)
       #TODO: fix proxy support
-      url = URI.parse("http://#{@url}")
-      req = Net::HTTP::Get.new(url.path)
+      uri = URI.parse("http://#{url}")
+      req = Net::HTTP::Get.new(uri.path)
       res = nil
-      Net::HTTP.start(url.host,url.port) {|http|
+      Net::HTTP.start(uri.host,uri.port) {|http|
         res = http.request(req)
       }
       res
+    end
+  end
+  
+  class Result
+    attr_reader :status_code, :linked_resources
+    def initialize(status_code, linked_resources)
+      @status_code = status_code
+      @linked_resources = linked_resources
     end
   end
 end
--- spec/explorer_spec.rb
+++ spec/explorer_spec.rb
@@ -2,16 +2,11 @@ require 'spec/spec_helper'
 require 'magellan/explorer'
 
 describe Magellan::Explorer do
+
   it "explorer should call back with the status code and other links to explore" do
-    status_code = nil
-    links = nil
-    explorer = Magellan::Explorer.new('canrailsscale.com/') do |code,l|
-      status_code = code
-      links = l
-    end
-    explorer.explore
-    links.should include('http://pagead2.googlesyndication.com/pagead/show_ads.js')
-    status_code.should eql('200')
+    result = Magellan::Explorer.new.explore('canrailsscale.com/')
+    result.linked_resources.should include('http://pagead2.googlesyndication.com/pagead/show_ads.js')
+    result.status_code.should eql('200')
   end
   
 end
