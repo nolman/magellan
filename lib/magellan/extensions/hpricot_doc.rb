c42a322945025a1da08c95ed2608b06deb161b65
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2009-03-20 16:27:04 -0700

    now pulls links to img src's as well

--- lib/magellan/extensions/hpricot_doc.rb
+++ lib/magellan/extensions/hpricot_doc.rb
@@ -1,6 +1,6 @@
 class Hpricot::Doc
   def links_to_other_documents
-    links_to_other_resources = [["a","href"],["script","src"]]
+    links_to_other_resources = [["a","href"],["script","src"],["img","src"]]
     links_to_other_resources.map {|links_to_other_resource| get_attributes(links_to_other_resource.first,links_to_other_resource.last)}.flatten
   end
   
--- spec/hpricot_doc_spec.rb
+++ spec/hpricot_doc_spec.rb
@@ -8,18 +8,25 @@ describe "Hpricot Extensions" do
     doc.links_to_other_documents.should be_empty
   end
   
-  it "should links based on script tags with src attritubes" do
+  it "should find links based on script tags with src attritubes" do
     doc = Hpricot("<script class=foo src='foozor'>something</script>")
     links_to_other_documents = doc.links_to_other_documents
     links_to_other_documents.size.should eql(1)
     links_to_other_documents.first.should eql("foozor")
   end
 
-  it "should links based on a tags with href attritubes" do
+  it "should find links based on a tags with href attritubes" do
     doc = Hpricot("<a class=foo href='bozo'>something</a>")
     links_to_other_documents = doc.links_to_other_documents
     links_to_other_documents.size.should eql(1)
     links_to_other_documents.first.should eql("bozo")
+  end
+
+  it "should find links based on img tags with src attritubes" do
+    doc = Hpricot("<img class=foo src='ohno' alt='whatever' />")
+    links_to_other_documents = doc.links_to_other_documents
+    links_to_other_documents.size.should eql(1)
+    links_to_other_documents.first.should eql("ohno")
   end
 
   it "should links based on a tags with href attritubes" do
