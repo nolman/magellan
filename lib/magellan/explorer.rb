bf53359884530c020d62790618e5fbb75e9baa35
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2009-03-20 13:44:12 -0700

    deleting unused method

--- lib/magellan/explorer.rb
+++ lib/magellan/explorer.rb
@@ -8,15 +8,5 @@ module Magellan
       doc = Hpricot(response)
       Result.new(url,"200",doc.links_to_other_documents)
     end
-    
-    def web_response(uri)
-      #TODO: fix proxy support
-      req = Net::HTTP::Get.new(uri.path)
-      res = nil
-      Net::HTTP.start(uri.host,uri.port) {|http|
-        res = http.request(req)
-      }
-      res
-    end
   end
 end
