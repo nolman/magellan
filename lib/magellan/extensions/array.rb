d3793ac39316c6e699c7d7543fdca1b6128d61b4
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2009-03-25 14:21:05 -0700

    changing the chunk to not use a index based approach

--- lib/magellan/extensions/array.rb
+++ lib/magellan/extensions/array.rb
@@ -3,7 +3,7 @@ class Array
     result = []
     number_of_chunks = (self.size.to_f / max_size).ceil
     for i in 0...number_of_chunks do
-      result[i] = self[i*max_size...(i+1)*max_size]
+      result << self[i*max_size...(i+1)*max_size]
     end
     result
   end
