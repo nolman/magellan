49fc3d25b17e2224c369190158c13f933838653d
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2009-03-25 14:02:26 -0700

    moved array extensions to its own file

--- lib/magellan.rb
+++ lib/magellan.rb
@@ -2,6 +2,7 @@ require 'magellan/cartographer'
 require 'magellan/explorer'
 require 'magellan/broken_link_tracker'
 require 'magellan/extensions/string'
+require 'magellan/extensions/array'
 require 'magellan/extensions/hpricot_doc'
 require 'magellan/extensions/open_uri'
 module Magellan
--- lib/magellan/cartographer.rb
+++ lib/magellan/cartographer.rb
@@ -46,14 +46,3 @@ module Magellan
     end
   end
 end
-
-class Array
-  def chunk(max_size)
-    result = []
-    number_of_chunks = (self.size.to_f / max_size).ceil
-    for i in 0...number_of_chunks do
-      result[i] = self[i*max_size...(i+1)*max_size]
-    end
-    result
-  end
-end
