44fdead26151381f3a27f3056308df0b581cb09b
Nolan Evans <nolanevans@nolan-evanss-macbook-pro-2.local>
2009-03-30 16:13:39 -0700

    added expected links tracker

--- lib/magellan/expected_links_tracker.rb
+++ lib/magellan/expected_links_tracker.rb
@@ -1,4 +1,17 @@
 module Magellan
   class ExpectedLinksTracker
+    attr_reader :errors
+    
+    def initialize(expected_patterns)
+      @errors = []
+      @expected_patterns = expected_patterns
+    end
+    
+    def update(time,result)
+      @expected_patterns.each do |pattern,expectation|
+        @errors << "#{result.url} did not contain a link to #{expectation}" unless result.linked_resources.include?(expectation)
+      end
+    end
+
   end
 end
--- spec/expected_links_tracker_spec.rb
+++ spec/expected_links_tracker_spec.rb
@@ -2,4 +2,24 @@ require File.dirname(__FILE__) + '/spec_helper'
 require 'magellan'
 
 describe Magellan::ExpectedLinksTracker do
+
+  it "should create a error message contianing the offending url and " do
+    tracker = Magellan::ExpectedLinksTracker.new([[/.*/,'/about_us.html']])
+    tracker.update(Time.now,Magellan::Result.new('200','/fozo',"/bar",[]))
+    tracker.errors.first.should include('/fozo')
+    tracker.errors.first.should include('/about_us.html')
+  end
+
+  it "should be able specify all resource should link to something" do
+    tracker = Magellan::ExpectedLinksTracker.new([[/.*/,'/about_us.html']])
+    tracker.update(Time.now,Magellan::Result.new('200','/zoro','/zoro',['/about_us.html']))
+    tracker.errors.empty?.should be_true
+    tracker.update(Time.now,Magellan::Result.new('200','/zoro','/zoro',['/about_fail_us.html']))
+    tracker.errors.empty?.should be_false
+  end
+  
+  it "only apply rules if they apply"
+  
+  it "should fail if a expectation was never met"
+  
 end
